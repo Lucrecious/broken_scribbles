1780edb33220b84fd861bedabd4afd4c4d94951b
Lucrecious <luca.duran@hotmail.com>
2020-05-11 05:33:28 -0400

    Fix typo

--- src/network/network.gd
+++ src/network/network.gd
@@ -133,8 +133,7 @@ master func enter_room(room_id : String) -> void:
 	rpc('_attempt_enter_room', sender_id, room_id, name)
 
 func _create_random_name() -> String:
-	var Constants = preload('res://src/game/constants.gd')
-	var adjective := Constants.FeelingsAdjectives.keys()[randi() % Constants.FeelingAdjectives.size()] as String
+	var adjective := Constants.FeelingsAdjectives.keys()[randi() % Constants.FeelingsAdjectives.size()] as String
 	adjective = adjective.to_lower()
 	
 	var animal := Constants.Animals.keys()[randi() % Constants.Animals.size()] as String
